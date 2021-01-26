5a7e976dd0ff5fd68e70fa5165333512373d8c54
Andrew M-Y <andrejs@gmail.com>
2021-01-26 19:27:01 +0200

    Try waiting for stomp on amq

--- travisci/bin/start.sh
+++ travisci/bin/start.sh
@@ -21,5 +21,25 @@ CONFIG_PATH=$(readlink -f ./travisci/conf)
 ./travisci/bin/rabbit-mq.sh "$RABBIT_VERSION" "$CONFIG_PATH"
 ./travisci/bin/apollo-mq.sh "$APLO_VERSION"
 
+function waitForStomp()
+{
+    ATTEMPTS=0
+    until docker run --rm ghcr.io/andrewmy/stomppy:latest stomp -H $1 -P $2; do
+        printf "wait for service %s:%s\n" $1 $2
+        ((ATTEMPTS++))
+        if [ $ATTEMPTS -ge $3 ]; then
+            printf "service is not running %s:%s\n" $1 $2
+            exit 1
+        fi
+
+        sleep 1
+    done
+
+    printf "service is online %s:%s\n" $1 $2
+}
+
+docker pull ghcr.io/andrewmy/stomppy:latest
+waitForStomp 127.0.0.1 61010 50
+
 echo ""
 echo "Brokers have been started for you, stop them by running ./travisci/bin/stop.sh"
