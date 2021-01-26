220f51065a1698c55ca7871bb4fab021b917a087
Andrew M-Y <andrejs@gmail.com>
2021-01-26 20:39:29 +0200

    Try clean again

--- travisci/bin/start.sh
+++ travisci/bin/start.sh
@@ -21,29 +21,5 @@ CONFIG_PATH=$(readlink -f ./travisci/conf)
 ./travisci/bin/rabbit-mq.sh "$RABBIT_VERSION" "$CONFIG_PATH"
 ./travisci/bin/apollo-mq.sh "$APLO_VERSION"
 
-function waitForStomp()
-{
-    ATTEMPTS=0
-    until docker run --rm ghcr.io/andrewmy/stomppy:latest stomp -H $1 -P $2 2>/dev/null; do
-        printf "wait for service %s:%s\n" $1 $2
-        ((ATTEMPTS++))
-        if [ $ATTEMPTS -ge $3 ]; then
-            printf "service is not running %s:%s\n" $1 $2
-            exit 1
-        fi
-
-        sleep 1
-    done
-
-    printf "service is online %s:%s\n" $1 $2
-}
-
-HOST_IP=$(docker network inspect bridge --format='{{range .IPAM.Config}}{{.Gateway}}{{end}}')
-
-echo "Host IP: $HOST_IP"
-
-docker pull ghcr.io/andrewmy/stomppy:latest
-waitForStomp "$HOST_IP" 61010 50
-
 echo ""
 echo "Brokers have been started for you, stop them by running ./travisci/bin/stop.sh"
